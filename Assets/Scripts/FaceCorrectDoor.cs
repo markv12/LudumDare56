6f2ea8bc4ef028e41d5980ee3d38b0e4e3fdec5d
Scott Steffes <swsteffes@gmail.com>
2024-10-05 18:42:18 -0500

    make gnome that faces the correct door

---
+++ Assets/Scripts/FaceCorrectDoor.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using UnityEngine;
+
+public class FaceCorrectDoor : MonoBehaviour {
+    public Transform mainT;
+    public Room room;
+    private void Start() {
+        Vector3 direction = room.CorrectDoorPos - mainT.position;
+        direction.y = 0;
+        if (direction != Vector3.zero) {
+            Quaternion rotation = Quaternion.LookRotation(direction);
+            mainT.rotation = rotation;
+        }
+    }
+}
