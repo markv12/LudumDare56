4978132ff3af98e4a84ee0c83e29347fa6db6a1d
Scott Steffes <swsteffes@gmail.com>
2024-10-06 11:47:55 -0500

    Add stuff

--- Assets/Scripts/TimeResetter.cs
+++ Assets/Scripts/TimeResetter.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 
 public class TimeResetter : MonoBehaviour
 {
-   
+    private void OnTriggerEnter(Collider other) {
+        if (!other.gameObject.IsPlayer()) {
+            Player.instance.ResetTime();
+        }
+    }
 }
