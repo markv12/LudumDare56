303fa8053d1e90da868f121dc5e67f8a2581dbab
Scott Steffes <swsteffes@gmail.com>
2024-10-06 14:48:26 -0500

    Add door texture

--- Assets/Scripts/Room/Room.cs
+++ Assets/Scripts/Room/Room.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Room : MonoBehaviour {
     public Rooms nextRoom;
     public RoomSpawnLocation[] spawnLocations; //Left Center Right
+    public RoomAnomaly[] anomalies; //Order doesn't matter
     private int correctDoorIndex;
     private void Awake() {
         correctDoorIndex = Random.Range(0, spawnLocations.Length);
@@ -10,6 +11,9 @@ public class Room : MonoBehaviour {
             RoomSpawnLocation rsl = spawnLocations[i];
             Rooms room = i == correctDoorIndex ? nextRoom : Rooms.StartRoom;
             rsl.Setup(room, gameObject);
+        }
+        if(anomalies.Length > 0) {
+            anomalies[Random.Range(0, anomalies.Length)].Activate();
         }
     }
 
