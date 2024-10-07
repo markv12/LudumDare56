71d30d693857714dca8d6f143a9b6d095beb79cd
Scott Steffes <swsteffes@gmail.com>
2024-10-07 02:57:49 -0500

    Add Anomalies

--- Assets/Scripts/KingGnomeFinishLine.cs
+++ Assets/Scripts/KingGnomeFinishLine.cs
@@ -5,8 +5,10 @@ public class KingGnomeFinish : InteractableObject {
     public SpriteRenderer mainRenderer;
     public AudioSource audioSource;
     public TMP_Text finishTimeText;
+    public Collider mainCollider;
     protected override void DoInteraction() {
         mainRenderer.enabled = false;
+        mainCollider.enabled = false;
         finishTimeText.text = Player.instance.RecordFinishTime().ToString("0.00");
         finishTimeText.gameObject.SetActive(true);
         audioSource.Play();
