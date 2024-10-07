3e3dd7156e0f599cbdb34d32c9b098dce21034a3
Scott Steffes <swsteffes@gmail.com>
2024-10-07 14:41:13 -0500

    all manner of stuff

--- Assets/Scripts/Room/DisappearAtDistance.cs
+++ Assets/Scripts/Room/DisappearAtDistance.cs
@@ -2,9 +2,19 @@ using UnityEngine;
 
 public class DisappearAtDistasnce : MonoBehaviour {
     public Transform mainT;
+    public SpriteRenderer mainRenderer;
+
+    private bool disappeared = false;
     private void Update() {
-        if (Player.instance != null && (Player.instance.t.position - mainT.position).sqrMagnitude < 50) {
-            Destroy(gameObject);
+        if (!disappeared && Player.instance != null && (Player.instance.t.position - mainT.position).sqrMagnitude < 50) {
+            disappeared = true;
+            Color startColor = mainRenderer.color;
+            Color endColor = startColor.SetA(0);
+            this.CreateWorldAnimRoutine(0.75f, (float progress) => {
+                mainRenderer.color = Color.Lerp(startColor, endColor, progress);
+            }, () => {
+                Destroy(gameObject);
+            });
         }
     }
 }
